d89fc8ab8f43a5f0e6632855952bb43893b1d482
skuwa229 <skuwa229@gmail.com>
2017-04-22 01:43:07 +0900

    add config file

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	datas := Utils.Read("schedule.csv")
-
+	calendarId := Utils.GetCalendarId()
 	for _, schedule := range datas {
 		s := Utils.SetSchedule(schedule)
-		Utils.CreateEvent(s)
+		Utils.CreateEvent(s, calendarId)
 	}
 
 	fmt.Println("finished!")
