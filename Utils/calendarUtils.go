98573876f826174d2398c4252574e914a76e10f4
konojunya <junya03dance@gmail.com>
2017-04-21 18:40:42 +0900

    スクリプト完成

--- Utils/calendarUtils.go
+++ Utils/calendarUtils.go
@@ -2,6 +2,7 @@ package Utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,7 +10,6 @@ import (
 	"os"
 	"os/user"
 	"path/filepath"
-  "fmt"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
@@ -20,14 +20,14 @@ import (
 const CALENDAR_ID string = "osaka.hal.iw13a727@gmail.com"
 
 func errorLog(message string, err error) {
-  if err != nil {
-    log.Fatalf(message, err)
-  }
+	if err != nil {
+		log.Fatalf(message, err)
+	}
 }
 
 func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
 	cacheFile, err := tokenCacheFile()
-  errorLog("Unable to get path to cached credential file.: ",err)
+	errorLog("Unable to get path to cached credential file.: ", err)
 
 	tok, err := tokenFromFile(cacheFile)
 	if err != nil {
@@ -47,7 +47,7 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 	}
 
 	tok, err := config.Exchange(oauth2.NoContext, code)
-  errorLog("Unable to retrieve token from web: ",err)
+	errorLog("Unable to retrieve token from web: ", err)
 
 	return tok
 }
@@ -78,7 +78,7 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 func saveToken(file string, token *oauth2.Token) {
 	fmt.Printf("Saving credential file to: %s\n", file)
 	f, err := os.Create(file)
-  errorLog("Unable to cache oauth token: ",err)
+	errorLog("Unable to cache oauth token: ", err)
 
 	defer f.Close()
 	json.NewEncoder(f).Encode(token)
@@ -86,39 +86,39 @@ func saveToken(file string, token *oauth2.Token) {
 
 func createEventData(ed map[string]string) *calendar.Event {
 
-  start_datatime := ed["Year"]+"-"+ed["Month"]+"-"+ed["Day"]+"T"+ed["Start"]+":00+09:00"
-  end_datatime := ed["Year"]+"-"+ed["Month"]+"-"+ed["Day"]+"T"+ed["End"]+":00+09:00"
+	start_datatime := ed["Year"] + "-" + ed["Month"] + "-" + ed["Day"] + "T" + ed["Start"] + ":00+09:00"
+	end_datatime := ed["Year"] + "-" + ed["Month"] + "-" + ed["Day"] + "T" + ed["End"] + ":00+09:00"
 
-  event := &calendar.Event{
-    Summary:     ed["Title"],
-    Location:    ed["Location"],
-    Start: &calendar.EventDateTime{
-      DateTime: start_datatime,
-      TimeZone: "Asia/Tokyo",
-    },
-    End: &calendar.EventDateTime{
-      DateTime: end_datatime,
-      TimeZone: "Asia/Tokyo",
-    },
-  }
+	event := &calendar.Event{
+		Summary:  ed["Title"],
+		Location: ed["Location"],
+		Start: &calendar.EventDateTime{
+			DateTime: start_datatime,
+			TimeZone: "Asia/Tokyo",
+		},
+		End: &calendar.EventDateTime{
+			DateTime: end_datatime,
+			TimeZone: "Asia/Tokyo",
+		},
+	}
 
-  return event
+	return event
 }
 
-func CreateEvent(ed map[string]string){
-  ctx := context.Background()
-  b,err := ioutil.ReadFile("client_secret.json")
-  errorLog("Unable to read client secret file: ",err)
+func CreateEvent(ed map[string]string) {
+	ctx := context.Background()
+	b, err := ioutil.ReadFile("client_secret.json")
+	errorLog("Unable to read client secret file: ", err)
 
-  config, err := google.ConfigFromJSON(b, calendar.CalendarScope)
-  errorLog("Unable to parse client secret file to config: ",err)
+	config, err := google.ConfigFromJSON(b, calendar.CalendarScope)
+	errorLog("Unable to parse client secret file to config: ", err)
 
-  client := getClient(ctx, config)
-  srv, err := calendar.New(client)
-  errorLog("Unable to retrieve calendar Client: ",err)
+	client := getClient(ctx, config)
+	srv, err := calendar.New(client)
+	errorLog("Unable to retrieve calendar Client: ", err)
 
-  _, err = srv.Events.Insert(CALENDAR_ID, createEventData(ed)).Do()
-  errorLog("Unable to create event. ",err)
+	_, err = srv.Events.Insert(CALENDAR_ID, createEventData(ed)).Do()
+	errorLog("Unable to create event. ", err)
 
-  fmt.Printf("Event created: %s\n", ed["Title"])
+	fmt.Printf("Set Schedule\n科目: %s\n教室: %s\n\n", ed["Title"], ed["Location"])
 }
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	ed := make(map[string]string)
 	datas := Utils.Read("schedule.csv")
 
-	for _,schedule := range datas {
+	for _, schedule := range datas {
 		ed["Title"] = schedule[0]
 		ed["Location"] = schedule[1]
 		ed["Year"] = schedule[2]
